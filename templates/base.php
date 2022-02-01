8db638b6eb15631ecb1b7e7d244830274a9b3e36
Psalm Diao <p.diao25@gmail.com>
2022-02-01 22:36:52 +0100

    Insert neko + enable adding more scripts and styles on templates

--- templates/base.php
+++ templates/base.php
@@ -1,11 +1,28 @@
+<?php
+  $styles = $styles ?? [];
+  $scripts = $scripts ?? [];
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <link rel="stylesheet" href="public/css/styles.css" />
+  <?php foreach ($styles as $style): ?>
+    <link rel="stylesheet" href="<?php echo $style; ?>" />
+  <?php endforeach; ?>
+
+  <?php foreach ($scripts as $script): ?>
+    <script src="<?php echo $script; ?>" defer></script>
+  <?php endforeach; ?>
 
   <title><?php echo $title ?? 'Home'; ?> • pdiao</title>
 </head>
 <body>
+  <script>NekoType="spooky"</script>
+  <h1 id="nl">
+    <script src="https://webneko.net/n20171213.js"></script>
+    <a href="https://webneko.net">Neko</a>
+  </h1>
+
   <div class="wrap">
     <main>
       <?php echo $content; ?>
@@ -22,7 +39,7 @@
 
       <nav>
         <ul>
-          <li><a href="/home">Home</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="/blog">Blog</a></li>
           <li><a href="/about-me">About</a></li>
         </ul>
